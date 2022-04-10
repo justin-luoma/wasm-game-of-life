33b158ca143c2088c04d3153abda3b63ec5620f2
Justin Luoma <justin@justinluoma.com>
2022-04-10 10:40:51 -0400

    feat: improved grid layout

--- www/index.js
+++ www/index.js
@@ -1,25 +1,54 @@
 import {Grid} from "wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
-const grid = Grid.new(75, 50);
-grid.init();
+const grid = Grid.new(75, 75);
+grid.spawn_glider_gun(30, 15);
+grid.spawn_pulsar(10, 35);
+grid.spawn_pentadecanthlon(25, 55);
 
 const renderLoop = () => {
     pre.textContent = grid.render();
-    grid.step_forward();
-
 
 
     requestAnimationFrame(renderLoop);
 };
 
 const spawn = () => {
+    grid.spawn_glider(2 ,2);
     setTimeout(() => {
-        grid.spawn_new()
-        spawn()
-    }, 1000);
+        spawn();
+    }, 15000);
 };
+
+const render = () => {
+    grid.step_forward();
+    setTimeout(() => {
+        render();
+    }, 250);
+};
+
+function randomize() {
+    grid.randomize();
+}
+
+function reset() {
+    grid.reset();
+}
+
+const setup = () => {
+    const randomizeBtn = document.getElementById("randomize");
+    randomizeBtn.addEventListener("click", randomize);
+
+    const resetBtn = document.getElementById("reset");
+    resetBtn.addEventListener("click", reset);
+};
+
+setup();
 
 requestAnimationFrame(renderLoop);
 
-setTimeout(() => spawn(), 1000);
+render();
+
+// setTimeout(() => spawn(), 1000);
+
+
